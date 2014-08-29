58064fd8923ac4fe41a85c46f64e7000cdd5267c
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-08-28 21:06:57 -0500

    Initial Commit

---
+++ APLProjects/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  APLProjects
+//
+//  Created by Dinesh Appavoo on 8/28/14.
+//  Copyright (c) 2014 Dinesh Appavoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
