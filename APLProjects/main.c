250b4eb5a3bf652f672c0b1f3d20e3742081a915
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-02 21:18:41 -0500

    Initial commit

--- APLProjects/main.c
+++ APLProjects/main.c
@@ -7,12 +7,29 @@
 //
 
 #include <stdio.h>
-
+void pointerExample();
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     printf("Hello, World!\n");
+    printf("Hi Dinesh\n");
+    
+    pointerExample();
     return 0;
 }
+void pointerExample()
+{
+    int a=43;
+    int b=44;
+    int* aPointer;
+    int* bPointer;
+    aPointer=&a;
+    bPointer=&b;
+    printf("%d\n",*aPointer);
+    aPointer=bPointer;
+    printf("%d\n",*aPointer);
+    printf("%d\n",*bPointer);
 
+    
+}
